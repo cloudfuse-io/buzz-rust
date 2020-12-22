e3749ee35c295a9953054535ff40d4cacdf3f1ad
rdettai <rdettai@gmail.com>
2020-12-22 15:10:08 +0100

    [test] test table search in logical plan

--- code/src/services/utils.rs
+++ code/src/services/utils.rs
@@ -33,3 +33,38 @@ fn as_table<'a, T: TableProvider + 'static>(plan: &'a LogicalPlan) -> Option<&'a
         None
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use super::*;
+    use crate::datasource::CatalogTable;
+    use arrow::datatypes::Schema;
+    use datafusion::datasource::empty::EmptyTable;
+    use datafusion::execution::context::ExecutionContext;
+    use datafusion::logical_plan::{sum, Expr};
+    use datafusion::scalar::ScalarValue;
+
+    #[test]
+    fn search_table_linear_plan() -> Result<()> {
+        let mut ctx = ExecutionContext::new();
+        let empty_table = EmptyTable::new(Arc::new(Schema::empty()));
+        let scalar_expr = Expr::Literal(ScalarValue::from(10));
+
+        let source_df = ctx.read_table(Arc::new(empty_table))?;
+
+        let filtered_df =
+            source_df.filter(scalar_expr.clone().eq(scalar_expr.clone()))?;
+
+        let grouped_df = filtered_df
+            .aggregate(vec![scalar_expr.clone()], vec![sum(scalar_expr.clone())])?;
+
+        assert!(find_table::<EmptyTable>(&source_df.to_logical_plan()).is_ok());
+        assert!(find_table::<EmptyTable>(&filtered_df.to_logical_plan()).is_ok());
+        assert!(find_table::<EmptyTable>(&grouped_df.to_logical_plan()).is_ok());
+        assert!(find_table::<CatalogTable>(&grouped_df.to_logical_plan()).is_err());
+
+        Ok(())
+    }
+}
