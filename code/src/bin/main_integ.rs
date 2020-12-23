ef767fa3ac25d22624e023f0a8be870708917e85
rdettai <rdettai@gmail.com>
2020-12-23 11:50:17 +0100

    [wip] logger experimentation

--- code/src/bin/main_integ.rs
+++ code/src/bin/main_integ.rs
@@ -1,18 +1,27 @@
+use tracing::info;
+use tracing_subscriber::fmt::{time::ChronoUtc, SubscriberBuilder};
+
 mod main_fuse;
 mod main_hbee;
 mod main_hcomb;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SubscriberBuilder::default().init();
+    // SubscriberBuilder::default()
+    //     .json()
+    //     .with_timer(ChronoUtc::rfc3339())
+    //     .init();
+
     tokio::select! {
         res = main_fuse::start_fuse("localhost", "localhost") => {
-            println!("fuse result: {:?}", res);
+            info!("fuse result: {:?}", res);
         }
         res = main_hbee::start_hbee_server() => {
-            println!("hbee server failed: {:?}", res);
+            info!("hbee server failed: {:?}", res);
         }
         res = main_hcomb::start_hcomb_server() => {
-            println!("hcomb server failed: {:?}", res);
+            info!("hcomb server failed: {:?}", res);
         }
     }
     Ok(())
