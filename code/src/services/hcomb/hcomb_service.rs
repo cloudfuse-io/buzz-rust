fcb32703226475f57135840e4636851f678553e5
rdettai <rdettai@gmail.com>
2021-01-12 19:38:07 +0100

    [feat] expire tasks automatically

--- code/src/services/hcomb/hcomb_service.rs
+++ code/src/services/hcomb/hcomb_service.rs
@@ -1,4 +1,7 @@
+use std::process::exit;
+use std::sync::atomic::{AtomicI64, Ordering};
 use std::sync::Arc;
+use std::time::Duration;
 
 use super::results_service::ResultsService;
 use crate::datasource::HCombTable;
@@ -17,16 +20,37 @@ use futures::{Stream, StreamExt};
 pub struct HCombService {
     results_service: Arc<ResultsService>,
     execution_context: ExecutionContext,
+    last_query: Arc<AtomicI64>, // timestamp of the last query in seconds
 }
+
+const TASK_EXPIRATION_SEC: i64 = 120;
 
 impl HCombService {
     pub fn new() -> Self {
         let config = ExecutionConfig::new()
             .with_batch_size(2048)
             .with_concurrency(1);
+        let last_query = Arc::new(AtomicI64::new(chrono::Utc::now().timestamp()));
+        let last_query_ref = Arc::clone(&last_query);
+        tokio::spawn(async move {
+            let mut interval = tokio::time::interval(Duration::from_secs(1));
+            loop {
+                interval.tick().await;
+                let elapsed = chrono::Utc::now().timestamp()
+                    - last_query_ref.load(Ordering::Relaxed);
+                if elapsed >= TASK_EXPIRATION_SEC {
+                    println!(
+                        "[hcomb] task expired after {}s of inactivity, shutting down...",
+                        elapsed
+                    );
+                    exit(0);
+                }
+            }
+        });
         Self {
             results_service: Arc::new(ResultsService::new()),
             execution_context: ExecutionContext::with_config(config),
+            last_query,
         }
     }
 
@@ -37,6 +61,8 @@ impl HCombService {
         plan: LogicalPlan,
     ) -> Result<(String, SendableRecordBatchStream)> {
         println!("[hcomb] execute query...");
+        self.last_query
+            .store(chrono::Utc::now().timestamp(), Ordering::Relaxed);
         let result_table = utils::find_table::<HCombTable>(&plan)?;
         let batch_stream = self
             .results_service
